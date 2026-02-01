37986d41b9602ec2747dfe570303ea53e6baa577
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-01-31 20:08:42 -0600

    Added a square pattern with alternating randomized colors

--- patternGeneration.js
+++ patternGeneration.js
@@ -3,6 +3,8 @@ const PATTERNMODES = {
     CIRCLES: "W",
 }
 
+const shapeSize = 20;
+
 let currentPatternMode = PATTERNMODES.SQUARES;
 
 function patternMode() {
@@ -17,7 +19,23 @@ function patternMode() {
 }
 
 function drawSquarePattern() {
+    stroke(0, 0, 0);
+    strokeWeight(1);
 
+    // Each time you switch to this mode we give it a new pattern
+    const colorOne = getRandomColor();
+    const colorTwo = getRandomColor();
+
+    // Draw left to right
+    for (let i = 0; i < height; i += shapeSize) {
+        for (let j = 0; j < width; j += shapeSize) {
+            fill(colorOne);
+            square(j, i, shapeSize);
+            j += shapeSize;
+            fill(colorTwo);
+            square(j, i, shapeSize);
+        }
+    }
 }
 
 function drawCirclePattern() {
