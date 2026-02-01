ee41a7f845db423e0b1cdd7da956826292d1d699
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-01-31 20:10:45 -0600

    Added circle mode

--- patternGeneration.js
+++ patternGeneration.js
@@ -39,5 +39,21 @@ function drawSquarePattern() {
 }
 
 function drawCirclePattern() {
+    stroke(0, 0, 0);
+    strokeWeight(1);
 
+    // Each time you switch to this mode we give it a new pattern
+    const colorOne = getRandomColor();
+    const colorTwo = getRandomColor();
+
+    // Draw left to right
+    for (let i = shapeSize / 2; i < height; i += shapeSize) {
+        for (let j = 0; j < width; j += shapeSize) {
+            fill(colorOne);
+            circle(j, i, shapeSize);
+            j += shapeSize;
+            fill(colorTwo);
+            circle(j, i, shapeSize);
+        }
+    }
 }
